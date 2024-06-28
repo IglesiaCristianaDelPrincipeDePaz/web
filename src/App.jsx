be7cd3b43ca5e98750cd2ad6b1414c545c1ae1ad
Emanuel Serveleon <serveleonemanuel@gmail.com>
2024-06-28 13:54:37 -0500

    Add Inter font and custom css reset for entire app
    Font: https://rsms.me/inter/
    CSS reset: https://www.joshwcomeau.com/css/custom-css-reset/

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Inicio from './pages/inicio';
 import Predicas from './pages/predicas';
 import Header from './components/Header';
+import './style.css';
 
 const App = () => {
   return (
