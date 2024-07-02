5bf8596c21ee4a9ed1e42ebb1eaf8c8e3b5e2dcd
Emanuel Serveleon <serveleonemanuel@gmail.com>
2024-07-02 11:40:36 -0500

    Render "predicas" with data from the api

--- client/src/pages/predicas/index.jsx
+++ client/src/pages/predicas/index.jsx
@@ -1,24 +1,17 @@
+import { useEffect, useState } from 'react';
 import './style.css';
 import PredicaCard from './components/PredicaCard';
 
-const predicas = [
-  {
-    id: 1,
-    titulo: 'Atrévete a ser diferente',
-    autor: 'Juan Serveleon',
-    fecha: '2024-06-23',
-    urlVideo: 'https://youtu.be/nowlHasXSC8?si=33Fl9zMLJycqjCfu',
-  },
-  {
-    id: 2,
-    titulo: 'Peleando por un sueño',
-    autor: 'Juan Serveleon',
-    fecha: '2024-06-16',
-    urlVideo: 'https://youtu.be/pkrXjNFjL6E?si=Z16VcbfusizcwawS',
-  },
-];
-
 const Predicas = () => {
+  const [predicas, setPredicas] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/predicas')
+      .then((res) => res.json())
+      .then((data) => setPredicas(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <h1 className="page-title">Prédicas</h1>
